ecba033a140f8ba0b6534fb2bf9cfee1591f4407
Prajval K <prajval22@iisertvm.ac.in>
2026-04-05 10:13:55 +0530

    update

--- openblas/build.sh
+++ openblas/build.sh
@@ -1,4 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-wget https://github.com/OpenMathLib/OpenBLAS/archive/refs/tags/v0.3.32.tar.gz
-tar -xf v0.3.32.tar.gz .
+set -e  # Exit on error
+
+# Variables
+VERSION="0.3.32"
+ARCHIVE="v${VERSION}.tar.gz"
+URL="https://github.com/OpenMathLib/OpenBLAS/archive/refs/tags/${ARCHIVE}"
+DIR="OpenBLAS-${VERSION}"
+
+# Download
+echo "Downloading OpenBLAS ${VERSION}..."
+wget -O ${ARCHIVE} ${URL}
+
+# Extract
+echo "Extracting..."
+tar -xf ${ARCHIVE}
+
+cd ${DIR}
+
+# Build (use all available cores)
+echo "Building OpenBLAS..."
+make -j$(nproc)
+
+# Optional: run tests
+# make test
+
+# Install (default: /opt/OpenBLAS or /usr/local if not overridden)
+echo "Installing OpenBLAS..."
+sudo make install
+
+echo "Done!"
