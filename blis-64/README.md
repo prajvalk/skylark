580d9faa45d6b8e70a418247e5d1310fecb5478c
prajvalk <prajval22@iisertvm.ac.in>
2026-04-18 03:14:15 +0530

    add blis

---
+++ blis-64/README.md
@@ -0,0 +1,1 @@
+AMD BLAS Library (BLIS) with 64bit Interface
---
+++ blis-64/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+# Get the source code
+mkdir -p skylark_wd
+pushd skylark_wd
+wget https://github.com/amd/blis/archive/refs/tags/5.2.2.tar.gz
+tar -xf 5.2.2.tar.gz
+
+cd blis-5.2.2
+./configure -i 64 -b 64 --prefix=/opt/blis-64 zen
+make -j
+
+# Install
+sudo make install
+
+popd
+rm -rf skylark_wd
